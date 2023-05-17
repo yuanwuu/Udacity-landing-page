356dadb492d282365ab4826b6c32d611dc1a14b5
Yuan Wu <wu.yuanf@gmail.com>
2023-05-17 10:39:46 -0400

    reorganized files and code

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-// 1. Create an empty element:
+// 1. Select an empty element:
 const navList = document.querySelector('ul');
-
 
 // 2. Create a list of text nodes from the navItem array:
 const navItem = ['home', 'coffee menu', 'about us', 'contact us'];
 
-
 // 3. Set the innerHTML of the ul element with the navItem array:
-navList.innerHTML = navItem.map(text => `<li><a href='/'>${text}</a></li>`).join('');
+navList.innerHTML = navItem.map(text => `<li><a href='#${text}'>${text}</a></li>`).join('');
 
 // 4. Append the ul element to the parent container:
 const navContainer = document.getElementById('nav-container');
 navContainer.appendChild(navList);
 
 
-// ------------------------- scorll ------------------------- 
+// ------------------------- scroll -------------------------
 const aboutEl = document.querySelector(".about");
-aboutEl.scrollIntoView({behavior:'smooth'});
-
 const contactEl = document.querySelector(".contact");
-contactEl.scrollIntoView({behavior:'smooth'});
 
-// 1. Create the element to add at the bottom of the page
-const bottomElement = document.createElement('div');
-bottomElement.innerHTML = '<span>&#8682</span>';
+aboutEl.scrollIntoView({ behavior: 'smooth' });
+contactEl.scrollIntoView({ behavior: 'smooth' });
 
-// 2. Append the element to the document body
-document.body.appendChild(bottomElement);
+// // 1. Create the element to add at the bottom of the page
+// const bottomElement = document.createElement('div');
+// bottomElement.innerHTML = '<span>&#8682</span>';
 
-// 3. Scroll back to the top of the page
-window.scrollTo({ top: 0, behavior: 'smooth' });
+// // 2. Append the element to the document body
+// document.body.appendChild(bottomElement);
 
-
-
-//------------------------ OnClick ------------------------ 
+// // 3. Scroll back to the top of the page
+// window.scrollTo({ top: 0, behavior: 'smooth' });
 
 
+
+//------------------------ Hover ------------------------ 
+
+const button = document.querySelector('button');
+
+button.addEventListener('mouseover', function() {
+  button.style.backgroundColor = 'white';
+  button.style.color = '#955f31';
+});
+
+button.addEventListener('mouseout', function() {
+  button.style.backgroundColor = '';
+  button.style.color = '';
+});
 
 
 
